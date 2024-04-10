37920eafb7edf713aff31d73dfc6ab575d1df5bf
AyushKumar9934 <83179865+AyushKumar9934@users.noreply.github.com>
2024-04-10 15:32:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+let fname=document.getElementById("fname")
+let lname=document.getElementById("lname")
+let phone=document.getElementById("tel")
+let email=document.getElementById("email");
+function give(){
+    alert(` First Name: ${fname.value} Last Name: ${lname.value} Phone Number: ${phone.value} Email ID: ${email.value}`)
+}
+document.getElementById("submit").addEventListener("click",give);
