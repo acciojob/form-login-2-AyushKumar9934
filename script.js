73dc0a6806119130e8868da86d0c86e14941fb0e
AyushKumar9934 <83179865+AyushKumar9934@users.noreply.github.com>
2024-05-02 11:23:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 //your JS code here. If required.
-let fname = document.getElementsByName("fname")[0];
-let lname = document.getElementsByName("lname")[0];
-let phone = document.getElementsByName("tel")[0];
-let email = document.getElementsByName("email")[0];
+let fname = document.getElementsByName("firstName")[0];
+let lname = document.getElementsByName("lastName")[0];
+let phone = document.getElementsByName("phoneNumber")[0];
+let email = document.getElementsByName("emailId")[0];
 
 function give() {
     alert(`First Name: ${fname.value} Last Name: ${lname.value} Phone Number: ${phone.value} Email ID: ${email.value}`)
